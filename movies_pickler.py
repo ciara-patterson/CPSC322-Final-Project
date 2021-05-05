bd0110e8dee91ca9752f6ba32307acefbfe527fd
Ciara Patterson <cpatterson@zagmail.gonzaga.edu>
2021-05-05 17:12:16 +0000

    trying to deploy

--- movies_pickler.py
+++ movies_pickler.py
@@ -7,8 +7,8 @@ import pickle
 
 # Importing the data and table and cols
 movies_fname = os.path.join("input_data", "movies.csv")
-movie_data = MyPyTable().load_from_file_no_encode(movies_fname)
-movies_table = MyPyTable().load_from_file(movies_fname)
+# movie_data = MyPyTable().load_from_file_no_encode(movies_fname)
+movies_table = MyPyTable().load_from_file(movies_fname, encode = 'cp1252')
 
 # Getting profit
 gross_profit = [movies_table.get_column('gross')[i] - movies_table.get_column('budget')[i] for i in range(len(movies_table.data))]
@@ -24,7 +24,7 @@ def test_folds(k, X_train_folds, X_test_folds, classifier, confusion_matrix = Fa
         feature_cols = ['budget', 'votes', 'genre', 'rating', 'score', 'star', 'director', 'writer']
         features = movies_table.get_key_columns(feature_cols)
         outcomes = profitted
-        
+
         # get the train and test set for linear_regression
         X_train = [features[j] for j in range(len(features)) if j in X_train_folds[i]]
         y_train = [outcomes[j] for j in range(len(outcomes)) if j in X_train_folds[i]]
@@ -44,8 +44,8 @@ def test_folds(k, X_train_folds, X_test_folds, classifier, confusion_matrix = Fa
         print('Confusion matrix')
         matrix = myeval.confusion_matrix(actuals, all_preds, labels)
         print(myutils.format_confusion_matrix(matrix, labels))
-       
-        
+
+
     else:
         # compute accuracy for the train and test sets
         acc = myutils.compute_accuracy(all_preds, actuals)
